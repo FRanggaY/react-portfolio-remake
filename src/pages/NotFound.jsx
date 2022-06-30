3b5c472873c1d28a665c37ec7eef6df867388ef1
FRanggaY <franciscusrangga@gmail.com>
2022-06-30 11:56:51 +0700

    create not found page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/css/Custom.css'
+
+function NotFound() {
+  return (
+    <div className="customBG d-flex justify-content-center align-items-center" style={{'height' : '100vh' }} >
+        <h1>404 Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFound
